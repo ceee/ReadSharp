40d07d8bd1eae6acb02b7a9545815f055eae29f7
Tobias Klika <cee@live.at>
2014-02-18 11:28:09 +0100

    fix for ZDNet

--- PortablePorts/NReadability/NReadabilityTranscoder.cs
+++ PortablePorts/NReadability/NReadabilityTranscoder.cs
@@ -144,7 +144,8 @@ namespace ReadSharp.Ports.NReadability
       { new Regex("^https?://(www.)?gizmodo.com", RegexOptions.IgnoreCase), ".post-container" },
       { new Regex("^https?://(www.)?it-scoop.com", RegexOptions.IgnoreCase), ".entry-content" },
       { new Regex("^https?://(m\\.|www\\.)?youtube.com", RegexOptions.IgnoreCase), "#watch-description-content" },
-      { new Regex("^https?://(www.)?vimeo.com", RegexOptions.IgnoreCase), ".description_wrapper" }
+      { new Regex("^https?://(www.)?vimeo.com", RegexOptions.IgnoreCase), ".description_wrapper" },
+      { new Regex("^https?://(www.)?zdnet.com", RegexOptions.IgnoreCase), ".storyBody" }
     };
 
     #endregion
